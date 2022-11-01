a03da5fc880bc22fe179a442108b7ddfb91ecedc
D.Farnham <danjf1210@gmail.com>
2022-11-01 17:58:38 +1100

    minor javascript updates

--- ReactPractice/index.js
+++ ReactPractice/index.js
@@ -35,3 +35,13 @@ ReactDOM.render(
         page, 
         document.getElementById("root")
 ); 
+
+/*
+Create a navbar in JSX: 
+    - Use semantic 'nav' element as the parent wrapper
+    - Have an h1 element with the name of the website
+    - Insert an unordered list for the other nav elements
+        - Inside the 'ul', have three 'li's' for 'Pricing', 'About' and 'Contact'
+    - Don't worry about styling yet 
+
+*/
